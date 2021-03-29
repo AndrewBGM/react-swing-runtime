bd28c47aa274993babea25d7a27b8a5af9577334
Andrew Bennett <andrewbgm@gmail.com>
2021-03-29 02:13:43 +0100

    cleanup

--- src/main/kotlin/io/github/andrewbgm/reactswingserver/ReactSwingServer.kt
+++ src/main/kotlin/io/github/andrewbgm/reactswingserver/ReactSwingServer.kt
@@ -2,19 +2,16 @@ package io.github.andrewbgm.reactswingserver
 
 import com.google.gson.GsonBuilder
 import io.github.andrewbgm.reactswingserver.gson.*
+import io.github.andrewbgm.reactswingserver.network.*
 import io.github.andrewbgm.reactswingserver.network.messages.*
 import io.javalin.Javalin
 import io.javalin.plugin.json.FromJsonMapper
 import io.javalin.plugin.json.JavalinJson
 import io.javalin.plugin.json.ToJsonMapper
-import java.awt.Container
-import javax.swing.JFrame
-import javax.swing.JMenuBar
 
 class ReactSwingServer {
+  private val bridge: ReactSwingBridge = ReactSwingBridge()
   private val app: Javalin by lazy { configureApp() }
-  private val topLevelFrames: MutableList<JFrame> = mutableListOf()
-  private val componentsById: MutableMap<Int, Container?> = mutableMapOf()
 
   fun start(
     port: Int,
@@ -26,202 +23,16 @@ class ReactSwingServer {
     app.stop()
   }
 
-  private fun appendChild(
-    parentId: Int,
-    childId: Int,
-  ) {
-    val parentComponent = componentsById[parentId] ?: error("No component #$parentId.")
-    val childComponent = componentsById[childId] ?: error("No component #$childId.")
-
-    if (parentComponent is JFrame) {
-      if (childComponent is JMenuBar) {
-        parentComponent.jMenuBar = childComponent
-      } else {
-        parentComponent.contentPane = childComponent
-      }
-    } else {
-      parentComponent.add(childComponent)
-    }
-  }
-
-  private fun appendChildToContainer(
-    containerId: Int,
-    childId: Int,
-  ) {
-    val childComponent = componentsById[childId] ?: error("No component #$childId.")
-
-    assert(containerId == 0) { "Container should always be 0." }
-    assert(childComponent is JFrame) { "Top level component #$childId must be JFrame." }
-
-    topLevelFrames.add(childComponent as JFrame)
-  }
-
-  private fun appendInitialChild(
-    parentId: Int,
-    childId: Int,
-  ) {
-    appendChild(parentId, childId)
-  }
-
-  private fun clearContainer(
-    containerId: Int,
-  ) {
-    assert(containerId == 0) { "Container should always be 0." }
-
-    topLevelFrames.forEach { it.dispose() }
-    topLevelFrames.clear()
-  }
-
-  private fun commitUpdate(
-    instanceId: Int,
-    prevProps: Map<String, Any?>,
-    nextProps: Map<String, Any?>,
-  ) {
-    val component = componentsById[instanceId] ?: error("No component #$instanceId.")
-    updateComponent(component, prevProps, nextProps)
-  }
-
-  private fun createInstance(
-    instanceId: Int,
-    type: String,
-    props: Map<String, Any?>,
-  ) {
-    val component = createComponent(type, props)
-    componentsById[instanceId] = component
-  }
-
-  private fun hideInstance(
-    instanceId: Int,
-  ) {
-    TODO("Not yet implemented")
-  }
-
-  private fun insertBefore(
-    parentId: Int,
-    childId: Int,
-    beforeChildId: Int,
-  ) {
-    val parentComponent = componentsById[parentId] ?: error("No component #$parentId.")
-    val childComponent = componentsById[childId] ?: error("No component #$childId.")
-    val beforeChildComponent = componentsById[childId] ?: error("No component #$beforeChildId.")
-
-    val idx = parentComponent.components.indexOf(beforeChildComponent)
-    if (parentComponent.components.contains(childComponent)) {
-      parentComponent.remove(childComponent)
-    }
-
-    parentComponent.add(childComponent, idx)
-  }
-
-  private fun insertInContainerBefore(
-    containerId: Int,
-    childId: Int,
-    beforeChildId: Int,
-  ) {
-    val childComponent = componentsById[childId] ?: error("No component #$childId.")
-    val beforeChildComponent = componentsById[childId] ?: error("No component #$beforeChildId.")
-
-    assert(containerId == 0) { "Container should always be 0." }
-    assert(childComponent is JFrame) { "Top level component #$childId must be JFrame." }
-    assert(beforeChildComponent is JFrame) { "Top level component #$beforeChildId must be JFrame." }
-
-    // This is probably unnecessary, but nice to keep the order at least.
-    val idx = topLevelFrames.indexOf(beforeChildComponent as JFrame)
-    topLevelFrames.add(idx, childComponent as JFrame)
-  }
-
-  private fun invokeCallback(
-    callbackId: Int,
-    args: List<Any?>,
-  ) {
-    TODO("Not yet implemented")
-  }
-
-  private fun removeChildFromContainer(
-    containerId: Int,
-    childId: Int,
-  ) {
-    val childComponent = componentsById[childId] ?: error("No component #$childId.")
-
-    assert(containerId == 0) { "Container should always be 0." }
-    assert(childComponent is JFrame) { "Top level component #$childId must be JFrame." }
-
-    childComponent as JFrame
-    childComponent.dispose()
-    topLevelFrames.remove(childComponent)
-  }
-
-  private fun removeChild(
-    parentId: Int,
-    childId: Int,
-  ) {
-    val parentComponent = componentsById[parentId] ?: error("No component #$parentId.")
-    val childComponent = componentsById[childId] ?: error("No component #$childId.")
-
-    if (parentComponent is JFrame) {
-      if (childComponent is JMenuBar) {
-        parentComponent.jMenuBar = null
-      } else {
-        parentComponent.contentPane = null
-      }
-    } else {
-      parentComponent.remove(childComponent)
-    }
-  }
-
-  private fun unhideInstance(
-    instanceId: Int,
-    props: Map<String, Any?>,
-  ) {
-    TODO("Not yet implemented")
-  }
-
-  private fun createComponent(
-    type: String,
-    props: Map<String, Any?>,
-  ): Container {
-    TODO("Not yet implemented")
-  }
-
-  private fun updateComponent(
-    component: Container,
-    prevProps: Map<String, Any?>,
-    nextProps: Map<String, Any?>,
-  ) {
-    TODO("Not yet implemented")
-  }
-
   private fun configureApp(): Javalin {
     configureGsonMappers()
 
     return Javalin
       .create()
       .ws("/ws") { ws ->
-        ws.onMessage { ctx ->
-          when (val message = ctx.message<IMessage>()) {
-            is AppendChildMessage -> appendChild(message.parentId, message.childId)
-            is AppendChildToContainerMessage ->
-              appendChildToContainer(message.containerId, message.childId)
-            is AppendInitialChildMessage -> appendInitialChild(message.parentId, message.childId)
-            is ClearContainerMessage -> clearContainer(message.containerId)
-            is CommitUpdateMessage ->
-              commitUpdate(message.instanceId, message.prevProps, message.prevProps)
-            is CreateInstanceMessage -> createInstance(message.instanceId,
-              message.type,
-              message.props)
-            is HideInstanceMessage -> hideInstance(message.instanceId)
-            is InsertBeforeMessage ->
-              insertBefore(message.parentId, message.childId, message.beforeChildId)
-            is InsertInContainerBeforeMessage ->
-              insertInContainerBefore(message.containerId, message.childId, message.beforeChildId)
-            is InvokeCallbackMessage -> invokeCallback(message.callbackId, message.args)
-            is RemoveChildFromContainerMessage ->
-              removeChildFromContainer(message.containerId, message.childId)
-            is RemoveChildMessage -> removeChild(message.parentId, message.childId)
-            is UnhideInstanceMessage -> unhideInstance(message.instanceId, message.props)
-            else -> error("Unsupported message $message.")
-          }
-        }
+        ws.onClose(bridge)
+        ws.onConnect(bridge)
+        ws.onError(bridge)
+        ws.onMessage(bridge)
       }
   }
 
--- src/main/kotlin/io/github/andrewbgm/reactswingserver/network/ReactSwingBridge.kt
+++ src/main/kotlin/io/github/andrewbgm/reactswingserver/network/ReactSwingBridge.kt
@@ -1,3 +1,152 @@
 package io.github.andrewbgm.reactswingserver.network
 
-class ReactSwingBridge
+import io.github.andrewbgm.reactswingserver.network.messages.*
+import io.javalin.websocket.WsCloseContext
+import io.javalin.websocket.WsCloseHandler
+import io.javalin.websocket.WsConnectContext
+import io.javalin.websocket.WsConnectHandler
+import io.javalin.websocket.WsErrorContext
+import io.javalin.websocket.WsErrorHandler
+import io.javalin.websocket.WsMessageContext
+import io.javalin.websocket.WsMessageHandler
+
+class ReactSwingBridge : WsCloseHandler, WsConnectHandler, WsErrorHandler, WsMessageHandler {
+  override fun handleClose(
+    ctx: WsCloseContext,
+  ) {
+
+  }
+
+  override fun handleConnect(
+    ctx: WsConnectContext,
+  ) {
+
+  }
+
+  override fun handleError(
+    ctx: WsErrorContext,
+  ) {
+
+  }
+
+  override fun handleMessage(
+    ctx: WsMessageContext,
+  ) {
+    when (val message = ctx.message<IMessage>()) {
+      is AppendChildMessage -> appendChild(message.parentId, message.childId)
+      is AppendChildToContainerMessage ->
+        appendChildToContainer(message.containerId, message.childId)
+      is AppendInitialChildMessage -> appendInitialChild(message.parentId, message.childId)
+      is ClearContainerMessage -> clearContainer(message.containerId)
+      is CommitUpdateMessage ->
+        commitUpdate(message.instanceId, message.prevProps, message.prevProps)
+      is CreateInstanceMessage -> createInstance(message.instanceId,
+        message.type,
+        message.props)
+      is HideInstanceMessage -> hideInstance(message.instanceId)
+      is InsertBeforeMessage ->
+        insertBefore(message.parentId, message.childId, message.beforeChildId)
+      is InsertInContainerBeforeMessage ->
+        insertInContainerBefore(message.containerId, message.childId, message.beforeChildId)
+      is InvokeCallbackMessage -> invokeCallback(message.callbackId, message.args)
+      is RemoveChildFromContainerMessage ->
+        removeChildFromContainer(message.containerId, message.childId)
+      is RemoveChildMessage -> removeChild(message.parentId, message.childId)
+      is UnhideInstanceMessage -> unhideInstance(message.instanceId, message.props)
+      else -> error("Unsupported message $message.")
+    }
+  }
+
+  private fun appendChild(
+    parentId: Int,
+    childId: Int,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  private fun appendChildToContainer(
+    containerId: Int,
+    childId: Int,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  private fun appendInitialChild(
+    parentId: Int,
+    childId: Int,
+  ) {
+    appendChild(parentId, childId)
+  }
+
+  private fun clearContainer(
+    containerId: Int,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  private fun commitUpdate(
+    instanceId: Int,
+    prevProps: Map<String, Any?>,
+    nextProps: Map<String, Any?>,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  private fun createInstance(
+    instanceId: Int,
+    type: String,
+    props: Map<String, Any?>,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  private fun hideInstance(
+    instanceId: Int,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  private fun insertBefore(
+    parentId: Int,
+    childId: Int,
+    beforeChildId: Int,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  private fun insertInContainerBefore(
+    containerId: Int,
+    childId: Int,
+    beforeChildId: Int,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  private fun invokeCallback(
+    callbackId: Int,
+    args: List<Any?>,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  private fun removeChildFromContainer(
+    containerId: Int,
+    childId: Int,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  private fun removeChild(
+    parentId: Int,
+    childId: Int,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  private fun unhideInstance(
+    instanceId: Int,
+    props: Map<String, Any?>,
+  ) {
+    TODO("Not yet implemented")
+  }
+}
