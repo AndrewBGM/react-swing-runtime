b6332bf91f6426856f7c12711ce6488cd823ade2
Andrew Bennett <andrewbgm@gmail.com>
2021-04-04 15:37:52 +0100

    add bridge

--- src/main/kotlin/io/github/andrewbgm/reactswingserver/ReactSwingServer.kt
+++ src/main/kotlin/io/github/andrewbgm/reactswingserver/ReactSwingServer.kt
@@ -5,12 +5,11 @@ import io.github.andrewbgm.reactswingserver.gson.*
 import io.github.andrewbgm.reactswingserver.messages.*
 import io.javalin.*
 import io.javalin.plugin.json.*
-import org.slf4j.*
 
 class ReactSwingServer {
-  private val logger = LoggerFactory.getLogger(ReactSwingServer::class.java)
-
   private val app: Javalin by lazy { configureApp() }
+
+  private val bridge: ReactSwingServerBridge = ReactSwingServerBridge()
 
   fun start(
     port: Int,
@@ -27,18 +26,7 @@ class ReactSwingServer {
 
     return Javalin
       .create()
-      .ws("/ws") { ws ->
-        ws.onConnect { logger.info("Connection opened.") }
-        ws.onMessage { handleMessage(it.message<Message>()) }
-        ws.onClose { logger.info("Connection closed.") }
-        ws.onError { logger.error("Connection error: ${it.error()}") }
-      }
-  }
-
-  private fun handleMessage(
-    message: Message,
-  ) {
-    logger.info("Message received: $message")
+      .ws("/ws") { ws -> bridge.attach(ws) }
   }
 
   private fun configureGsonMappers() {
