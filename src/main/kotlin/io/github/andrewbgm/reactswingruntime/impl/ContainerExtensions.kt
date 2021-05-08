7279bff4febc5eab6ef86bae27c8100bfec0a4be
Andrew Bennett <andrewbgm@gmail.com>
2021-05-08 22:27:12 +0100

    special handling for menu mutations

--- src/main/kotlin/io/github/andrewbgm/reactswingruntime/impl/ContainerExtensions.kt
+++ src/main/kotlin/io/github/andrewbgm/reactswingruntime/impl/ContainerExtensions.kt
@@ -1,15 +1,25 @@
 package io.github.andrewbgm.reactswingruntime.impl
 
 import java.awt.*
+import javax.swing.*
 
 fun Container.insertBefore(
   child: Container,
   beforeChild: Container
 ) {
-  if (components.contains(child)) {
-    remove(child)
-  }
+  if (this is JMenu) {
+    if (menuComponents.contains(child)) {
+      remove(child)
+    }
 
-  val idx = components.indexOf(beforeChild)
-  add(child, idx)
+    val idx = menuComponents.indexOf(beforeChild)
+    add(child, idx)
+  } else {
+    if (components.contains(child)) {
+      remove(child)
+    }
+
+    val idx = components.indexOf(beforeChild)
+    add(child, idx)
+  }
 }
